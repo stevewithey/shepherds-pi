0d9d0cc9d44556a2a18eacb8729b5b96e5d2107d
Steve Withey <stevewithey@gmail.com>
2019-07-07 22:34:00 +0100

    Refactoring and testing switching off

---
+++ Shepherds.Pie/ewanCommands.py
@@ -0,0 +1,39 @@
+import gpiozero
+import time
+
+# Constants
+SLEEP_TIME = 1
+TRIGGER_DURATION = 0.5
+SWITCH_OFF_HOLD_SECONDS = 3
+
+# Assign the pins
+RELAY_1_PIN = 18
+RELAY_2_PIN = 23
+RELAY_3_PIN = 24
+RELAY_4_PIN = 25
+
+# Create the relay 'switches'
+sound1 = gpiozero.OutputDevice(RELAY_1_PIN, active_high=False, initial_value=False)
+sound2 = gpiozero.OutputDevice(RELAY_2_PIN, active_high=False, initial_value=False)
+sound3 = gpiozero.OutputDevice(RELAY_3_PIN, active_high=False, initial_value=False)
+sound4 = gpiozero.OutputDevice(RELAY_4_PIN, active_high=False, initial_value=False)
+
+def trigger_sound(soundRelay):
+    soundRelay.on()
+    time.sleep(TRIGGER_DURATION)
+    soundRelay.off()
+
+def reset_relays():
+    print("Turning off relay 1")
+    sound1.off()
+    print("Turning off relay 2")
+    sound2.off()
+    print("Turning off relay 3")
+    sound3.off()
+    print("Turning off relay 4")
+    sound4.off()
+
+def stop_playback():
+    sound1.on()
+    time.sleep(SWITCH_OFF_HOLD_SECONDS)
+    sound1.off()
--- Shepherds.Pie/testRelays.py
+++ Shepherds.Pie/testRelays.py
@@ -1,59 +1,33 @@
 import gpiozero
 import time
+import ewanCommands
 
 # Constants
 SLEEP_TIME = 1
 TRIGGER_DURATION = 0.5
 
-# Assign the pins
-RELAY_1_PIN = 18
-RELAY_2_PIN = 23
-RELAY_3_PIN = 24
-RELAY_4_PIN = 25
-
-# Create the relay 'switches'
-sound1 = gpiozero.OutputDevice(RELAY_1_PIN, active_high=False, initial_value=False)
-sound2 = gpiozero.OutputDevice(RELAY_2_PIN, active_high=False, initial_value=False)
-sound3 = gpiozero.OutputDevice(RELAY_3_PIN, active_high=False, initial_value=False)
-sound4 = gpiozero.OutputDevice(RELAY_4_PIN, active_high=False, initial_value=False)
-
-def reset_relays():
-    print("Turning off relay 1")
-    sound1.off()
-    print("Turning off relay 2")
-    sound2.off()
-    print("Turning off relay 3")
-    sound3.off()
-    print("Turning off relay 4")
-    sound4.off()
-
-
-def trigger_sound(soundRelay):
-    soundRelay.on()
-    time.sleep(TRIGGER_DURATION)
-    soundRelay.off()
-
 def test_loop():
-    reset_relays()
+    ewanCommands.reset_relays()
     while 1:
         print("Triggering relay 1")
-        trigger_sound(sound1)
+        ewanCommands.trigger_sound(ewanCommands.sound1)
         time.sleep(SLEEP_TIME)
 
         print("Triggering relay 2")
-        trigger_sound(sound2)
+        ewanCommands.trigger_sound(ewanCommands.sound2)
         time.sleep(SLEEP_TIME)
 
         print("Triggering relay 3")
-        trigger_sound(sound3)
+        ewanCommands.trigger_sound(ewanCommands.sound3)
         time.sleep(SLEEP_TIME)
 
         print("Triggering relay 4")
-        trigger_sound(sound4)
+        ewanCommands.trigger_sound(ewanCommands.sound4)
         time.sleep(SLEEP_TIME)
 
 if __name__ == "__main__":
     try:
-        test_loop()
+        ewanCommands.stop_playback()
+        #test_loop()
     except KeyboardInterrupt:
-        reset_relays()
+        ewanCommands.reset_relays()
